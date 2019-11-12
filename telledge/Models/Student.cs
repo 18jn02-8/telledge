1964a9466315a9821e5570d9d71c7a343569161b
kurosawa <acb.vdf>
2019-11-12 12:26:05 +0900

    各クラスの属性の修正

--- telledge/Models/Student.cs
+++ telledge/Models/Student.cs
@@ -8,13 +8,31 @@ namespace telledge.Models
 {
     public class Student : Controller
     {
+        //生徒ID
         public int id { get; set; }
+
+        //生徒名
         public String name { get; set;}
+
+        //生徒メールアドレス
         public String mailaddress { get; set; }
+
+        //生徒プロフィール画像
+        public String profileImage { get; set; }
+
+        //生徒スカイプID
         public String skypeId { get; set; }
+
+        //パスワードダイジェスト
         public int passwordDigest { get; set; }
+
+        //二段階認証の有無
         public bool is2FA { get; set; }
+
+        //ポイント
         public int point { get; set; }
+
+        //生徒退会日
         public DateTime inactiveDate {get; set;}
     }
 }
--- telledge/Models/Teacher.cs
+++ telledge/Models/Teacher.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace telledge.Models
 {
-    public class Teacher
+    public class Teacher : Controller
     {
         //講師ID
         public int id { set; get; }
