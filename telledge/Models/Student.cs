8b6fe21915dac26583269f005652e0be185b5a76
Kazuya Furukawa <kazuyafuruikawa@yahoo.co.jp>
2019-11-12 10:17:17 +0900

    Student

---
+++ telledge/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace telledge.Models
+{
+    public class Student : Controller
+    {
+        public int id {get;set;}
+        public String name { get; set;}
+        public String mailaddress { get; set; }
+        public String skypeid { get; set; }
+        public int passworddigest { get; set; }
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
