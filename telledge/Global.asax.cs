4710584c407f732e315ecfaa61e569ad37a4f1de
Tatsuki <18jn0247@jec.ac.jp>
2019-11-14 10:53:59 +0900

    アクタでURLをネストする設定を追加

--- telledge/Global.asax.cs
+++ telledge/Global.asax.cs
@@ -16,18 +16,18 @@ namespace telledge
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            routes.MapRoute(
+                "Student",
+                "student/{controller}/{action}/{id}",
+                new { id = UrlParameter.Optional },
+                new[] { "telledge.Controllers.Student" }
+            );
             routes.MapRoute(
                 "Default", // ルート名
                 "{controller}/{action}/{id}", // パラメーター付きの URL
                 new { controller = "Home", action = "Index", id = UrlParameter.Optional } // パラメーターの既定値
             );
-            routes.MapRoute(
-                "Student",
-                "student/{controller}/{action}/{id}",
-                new { id = UrlParameter.Optional },
-                namespaces: new[] { "telledge.Controllers.Student" }
-            );
+
         }
 
         protected void Application_Start()
