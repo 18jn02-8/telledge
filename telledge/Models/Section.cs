b45f19422bf29830221a153b951be0f0d27803f5
Tatsuki Watanabe <18jn0247@jec.ac.jp>
2019-11-20 15:31:58 +0900

    フィールドの定義を追加

--- telledge/Models/Section.cs
+++ telledge/Models/Section.cs
@@ -7,5 +7,12 @@ namespace telledge.Models
 {
     public class Section
     {
+        public int roomId { get; set; }
+        public int studentId { get; set; }
+        public String request { get; set; }
+        public int valuation { get; set; }
+        public int order { get; set; }
+        public DateTime beginTime { get; set; }
+        public int talkTime { get; set; }
     }
 }
