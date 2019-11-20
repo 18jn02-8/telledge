66d7aeb0d73aee323539f1ec6d31c90c7a4e0934
Tatsuki Watanabe <18jn0247@jec.ac.jp>
2019-11-20 15:44:21 +0900

    sqlをroomからコピー

--- telledge/Models/Section.cs
+++ telledge/Models/Section.cs
@@ -1,7 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+
 using System.Web;
+
+using System.Data;
+
+using System.Text;
+
+using System.Security.Cryptography;
+using System.Data.SqlClient;
+using System.Configuration;
+using telledge.Models;
 
 namespace telledge.Models
 {
@@ -14,5 +22,42 @@ namespace telledge.Models
         public int order { get; set; }
         public DateTime beginTime { get; set; }
         public int talkTime { get; set; }
+
+        public Room getRoom()
+        {
+            Room retRoom = null;
+            string cstr = ConfigurationManager.ConnectionStrings["Db"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(cstr))
+            {
+                if (retStudent.inactiveDate != null)
+                {
+                    return null;
+                }
+                string sql = "select * from Student where id = @id";
+                SqlDataAdapter adapter = new SqlDataAdapter(sql, connection);
+                adapter.SelectCommand.Parameters.Add("@id", SqlDbType.VarChar);
+                DataSet ds = new DataSet();
+                int cnt = adapter.Fill(ds, "Student");
+                if (cnt != 0)
+                {
+                    retStudent = new Student();
+                    DataTable dt = ds.Tables["Student"];
+                    retStudent.passwordDigest = (Byte[])dt.Rows[0]["passwordDigest"];
+                }
+
+                byte[] input = Encoding.ASCII.GetBytes(password);
+                SHA256 sha = new SHA256CryptoServiceProvider();
+                byte[] hash_sha256 = sha.ComputeHash(input);
+                if (retStudent.passwordDigest == hash_sha256)
+                {
+                    HttpContext.Current.Session["Student"] = retStudent;
+                    return retStudent;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
     }
 }
