0f52741a092ade040c756ab7a98d878561dc4022
Tatsuki <18jn0247@jec.ac.jp>
2019-11-21 11:13:01 +0900

    Auto stash before rebase of "origin/master"

---
+++ UnitTest/Section/getRoom.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.Section
+{
+    [TestClass]
+    public class getRoom
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
