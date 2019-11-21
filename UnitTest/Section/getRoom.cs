f073cb2927a38897aeb7a7eb6346fc02b57ad404
Tatsuki <18jn0247@jec.ac.jp>
2019-11-21 11:37:29 +0900

    テストファイルの作成

--- UnitTest/Section/getRoom.cs
+++ UnitTest/Section/getRoom.cs
@@ -1,14 +1,25 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using telledge.Models;
 
-namespace UnitTest.Section
+namespace UnitTest.Sections
 {
     [TestClass]
     public class getRoom
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Success()
         {
+            Section section = new Section();
+            section.roomId = 1;
+            Room room = section.getRoom();
+            Assert.IsNotNull(room);
+        }
+        public void failed()
+        {
+            Section section = new Section();
+            Room room = section.getRoom();
+            Assert.IsNull(room);
         }
     }
 }
