8a480943f7901efff7cea9cd45b153102881ee96
Tatsuki <18jn0247@jec.ac.jp>
2019-11-21 12:29:43 +0900

    コンフリクト修正

---
+++ UnitTest/Room/getRooms.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using telledge.Models;
+
+namespace UnitTest.Rooms
+{
+    [TestClass]
+    public class getRooms
+    {
+        [TestMethod]
+        public void Success()
+        {
+            
+        }
+        [TestMethod]
+        public void Failed()
+        {
+
+        }
+    }
+}
