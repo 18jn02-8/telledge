65de98d1e56d0120f611813d4adc251ca35dc621
kurosawa <acb.vdf>
2019-11-21 10:36:45 +0900

    Teacgerクラスのテストコードの追加

--- UnitTest/Teacher/Teacher.cs
+++ UnitTest/Teacher/Teacher.cs
@@ -35,5 +35,23 @@ namespace telledge.Tests.Models
             bool test = teacher.create();
             Assert.IsTrue(test);
         }
+        [TestMethod]
+        public void TestCreateFailed()
+        {
+            Teacher teacher = new Teacher();
+            teacher.name = "Test";
+            teacher.sex = 1;
+            teacher.profileImage = "Gafeokfwaoefa.pix";
+            teacher.age = 20;
+            teacher.intoroduction = "Japan";
+            teacher.setPassword("password");
+            teacher.mailaddress = "OARO@jec.ac.jp";
+            teacher.is2FA = false;
+            teacher.point = 0;
+            teacher.address = "Japan Sinjuku Kabukityo";
+            teacher.nationality = "???";
+            bool test = teacher.create();
+            Assert.IsFalse(test);
+        }
     }
 }
