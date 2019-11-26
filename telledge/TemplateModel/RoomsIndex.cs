c32f91b09ec9475d429a54f2d339be1ddaaaa091
Tatsuki <18jn0247@jec.ac.jp>
2019-11-26 12:28:43 +0900

    rooms/index追加

---
+++ telledge/TemplateModel/RoomsIndex.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using telledge.Models;
+
+namespace telledge.TemplateModel
+{
+    public class RoomsIndex
+    {
+        public Room[] rooms;
+    }
+}
