882bf9f1d31ab09afedc99ee12cf24ab9580d0ed
Kazuya Furukawa <kazuyafuruikawa@yahoo.co.jp>
2019-11-26 10:26:13 +0900

    RoomsCreateコントローラー作成

---
+++ telledge/Controllers/Teachers/RoomsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace telledge.Controllers.Teachers
+{
+    public class RoomsController : Controller
+    {
+        // GET: Rooms
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View("Create");
+
+        }
+    }
+}
