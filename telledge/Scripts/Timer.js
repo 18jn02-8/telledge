0234e10b9161227ca0b1bf03d2bd09d7c1504a3c
kurosawa <acb.vdf>
2019-12-03 12:18:53 +0900

    Javascriptファイルの作成

---
+++ telledge/Scripts/Timer.js
@@ -0,0 +1,12 @@
+function timer() {
+	const timers = $('#timer').text();
+	var id = setInterval(function () {
+		var timer = (parseInt(timers) * 60) - 1;
+		var min = timer / 60;
+		var sec = timer % 60;
+		$('#timer').text(timer);
+		if (timer <= 0) {
+			clearInterval(id);　//idをclearIntervalで指定している
+		}
+	}, 1000);
+}
