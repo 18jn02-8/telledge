cab636930a912fcac4f8b212d587509e256c6f25
kurosawa <acb.vdf>
2019-12-05 11:38:59 +0900

    TimerのJs修正

--- telledge/Scripts/Timer.js
+++ telledge/Scripts/Timer.js
@@ -1,12 +1,34 @@
-function timer() {
-	const timers = $('#timer').text();
-	var id = setInterval(function () {
-		var timer = (parseInt(timers) * 60) - 1;
-		var min = timer / 60;
-		var sec = timer % 60;
-		$('#timer').text(timer);
-		if (timer <= 0) {
-			clearInterval(id);　//idをclearIntervalで指定している
-		}
-	}, 1000);
+class Timer{
+	constructor(mintimer, overtimer) {
+		super(mintimer, overtimer);
+	}
+
+	minTime() {
+		var count = 0;
+		var id = setInterval(function () {
+			mintimer = (parseInt(mintimer) * 60) - count;
+			var min = mintimer / 60;
+			var sec = mintimer % 60;
+			$('#lu').Text(parseInt(min) + ":" + parseInt(sec));
+			count++;
+			if (mintimer <= 0) {
+				clearInterval(id);　//idをclearIntervalで指定している
+				overTime();
+			}
+		}, 1000);
+	}
+
+	overTime() {
+		var count = 0;
+		var id = setInterval(function () {
+			overtimer = (parseInt(overtimer) * 60);
+			var min = overtimer / 60;
+			var sec = overtimer % 60;
+			$('#lu').Text(parseInt(min) + ":" + parseInt(sec));
+			count++;
+			if (overtimer < count) {
+				clearInterval(id);　//idをclearIntervalで指定している
+			}
+		}, 1000);
+	}
 }
