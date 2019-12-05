c65fd39f1eaab6607c58ee14c0a511544916b972
kurosawa <acb.vdf>
2019-12-05 13:39:36 +0900

    Timerの修正

--- telledge/Scripts/Timer.js
+++ telledge/Scripts/Timer.js
@@ -2,33 +2,39 @@ class Timer{
 	constructor(mintimer, overtimer) {
 		super(mintimer, overtimer);
 	}
-
 	minTime() {
 		var count = 0;
 		var id = setInterval(function () {
 			mintimer = (parseInt(mintimer) * 60) - count;
 			var min = mintimer / 60;
 			var sec = mintimer % 60;
-			$('#lu').Text(parseInt(min) + ":" + parseInt(sec));
+			$('#calltime').Text(parseInt(min) + ":" + parseInt(sec));
 			count++;
 			if (mintimer <= 0) {
 				clearInterval(id);　//idをclearIntervalで指定している
-				overTime();
+				return 0;
 			}
 		}, 1000);
+		return 1;
 	}
-
 	overTime() {
 		var count = 0;
 		var id = setInterval(function () {
 			overtimer = (parseInt(overtimer) * 60);
 			var min = overtimer / 60;
 			var sec = overtimer % 60;
-			$('#lu').Text(parseInt(min) + ":" + parseInt(sec));
+			$('#calltime').Text(parseInt(min) + ":" + parseInt(sec));
 			count++;
 			if (overtimer < count) {
 				clearInterval(id);　//idをclearIntervalで指定している
 			}
 		}, 1000);
+	}
+
+	startTime() {
+		flag = minTime();
+		if (flag == 0) {
+			overTime();
+		}
 	}
 }
