2773f21fa5067af4fa724fc81ed18df5c0225e7e
Kazuya Furukawa <kazuyafuruikawa@yahoo.co.jp>
2019-12-05 12:19:51 +0900

    TeacherRoomsCreateAction

--- telledge/Controllers/Teachers/RoomsController.cs
+++ telledge/Controllers/Teachers/RoomsController.cs
@@ -27,4 +27,37 @@ namespace telledge.Controllers.Teachers
 			return View("/Views/Teachers/Rooms/call.cshtml", model);
 		}
 	}
+        }
+		[HttpPost]
+		public ActionResult Create(int teacherId,String roomName,String tag,String description,int worstTime,int extensionTime,int point,DateTime endScheduleTime, DateTime beginTime, DateTime endTime)
+		{
+			Room room = new Room();
+			room.teacherId = teacherId;
+			room.roomName = roomName;
+			room.tag = tag;
+			room.description = description;
+			room.worstTime = worstTime;
+			room.extensionTime = extensionTime;
+			room.point = point;
+			room.endScheduleTime = endScheduleTime;
+			room.beginTime = beginTime;
+			room.endTime = endTime;
+
+			bool ret = room.create();
+			if(ret == true)
+			{
+				return RedirectToAction("call", "Rooms");
+			}
+			else
+			{
+				return View("Create");
+			}
+
+		}
+		public ActionResult index()
+        {
+            var model = Room.getRooms();
+            return View("/Views/Teachers/Rooms/call.cshtml", model);
+        }
+    }
 }
