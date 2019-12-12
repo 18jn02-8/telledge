d0b0cf105b9a8d11e3a02f75f4f0ba7713e1c5fc
Tatsuki Watanabe <42799529+Tatsuki5274@users.noreply.github.com>
2019-12-12 13:41:38 +0900

    Update Inquiry.cs

--- telledge/Models/Inquiry.cs
+++ telledge/Models/Inquiry.cs
@@ -10,7 +10,7 @@ namespace telledge.Models
 		public int id; //問い合わせID
 		public String inquiryContent;//問い合わせ内容
 		public DateTime inquiryTime;//問い合わせ時間
-		public String sender;//送信者名
+		public String senderName;//送信者名
 		public String senderContent;//送信内容
 		public int replierId;//返信者ID
 		public String repliersContent;//返信内容
