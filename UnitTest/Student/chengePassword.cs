dc37c9f838967d0fcc515af4572b7f42d3c20e68
kurosawa <acb.vdf>
2019-12-12 16:13:45 +0900

    ChengePasswordのメソッドの完成、テストの作成

---
+++ UnitTest/Student/chengePassword.cs
@@ -0,0 +1,28 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using telledge.Models;
+
+namespace UnitTest.Students
+{
+    [TestClass]
+    public class StudentchengePassword
+    {
+        [TestMethod]
+        public void TestchengePassword()
+        {
+            Student student = new Student();
+			student.id = 2;
+			student.setPassword("password");
+			bool test = student.chengePassword("password", "newpass");
+            Assert.IsTrue(test);
+        }
+        [TestMethod]
+        public void TestchengePasswordFailed()
+        {
+			Student student = new Student();
+			student.id = 2;
+			student.setPassword("password");
+			bool test = student.chengePassword("pappappa", "newpass");
+			Assert.IsFalse(test);
+        }
+    }
+}
