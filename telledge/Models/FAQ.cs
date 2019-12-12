94d4512b0b606caa238d103d5c9fd8bac101717b
kurosawa <acb.vdf>
2019-12-12 14:19:02 +0900

    FAQクラスのCreateメソッドの完成

--- telledge/Models/FAQ.cs
+++ telledge/Models/FAQ.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -13,5 +15,34 @@ namespace telledge.Models
 		public String question;
 		//FAQ回答
 		public String answer;
+
+		public bool create()
+		{
+			bool check = false;
+			string cstr = ConfigurationManager.ConnectionStrings["Db"].ConnectionString;
+			using (var connection = new SqlConnection(cstr))
+			using (var command = connection.CreateCommand())
+			{
+				try
+				{
+					connection.Open();
+					command.CommandText = "Insert Into FAQ Values (@question,@answer) ";
+					command.Parameters.Add(new SqlParameter("@question", question));
+					command.Parameters.Add(new SqlParameter("@answer", answer));
+					int cnt = command.ExecuteNonQuery();
+					connection.Close();
+					if(cnt != 0)
+					{
+						check = true;
+					} 
+				}
+				catch (SqlException e)
+				{
+					//入力情報が足りないメッセージを吐く
+					return check;
+				}
+			}
+			return check;
+		}
 	}
 }
