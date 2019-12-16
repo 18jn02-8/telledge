d9ae03ce1a508034c7c765da14db20472fc49f22
KAZUYAFURUKAWA <KAZUYAFURUKAWA>
2019-12-16 15:46:07 +0900

    テストコードの作成

--- UnitTest/Inquiry/find.cs
+++ UnitTest/Inquiry/find.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using telledge.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace UnitTest.Inquiry
+namespace UnitTest.Inquirys
 {
+	[TestClass]
 	class find
 	{
+		[TestMethod]
+		public void success()
+		{
+			Inquiry inquiry = Inquiry.find(1);
+			Assert.IsNotNull(inquiry);
+		}
 	}
 }
