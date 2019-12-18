b0b06cb2e1f475dea11374585e634fe380eeb102
KAZUYAFURUKAWA <doiajdpoia>
2019-12-18 16:32:51 +0900

    テストコードの作成

---
+++ UnitTest/Room/getRooms(String tag).cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using telledge.Models;
+
+
+namespace UnitTest.Rooms
+{
+	[TestClass]
+	public class getRooms_String_tag_
+	{
+		[TestMethod]
+		public void succsess()
+		{
+			Room room = new Room();
+			room.tag = "tag";
+			Room[] rooms = Room.getRooms(room.tag);
+			Assert.IsNotNull(rooms);
+		}
+	}
+}
