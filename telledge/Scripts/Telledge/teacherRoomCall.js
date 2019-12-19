05b35b8f6c418dd4e67bdebdef071fd32c136886
Tatsuki <18jn0247@jec.ac.jp>
2019-12-19 10:43:44 +0900

    表示変更の処理を記述

--- telledge/Scripts/Telledge/teacherRoomCall.js
+++ telledge/Scripts/Telledge/teacherRoomCall.js
@@ -1,1 +1,13 @@
-﻿
+let timer = new Timer(mintime, overtime);
+timer.setCallback(Status.Essential, () => {
+	$('#timer-title').text('残り時間');
+	$('#timer-count').css('color', 'black');
+	$('#timer-title').css('color', 'black');
+});
+timer.setCallback(Status.Extend, () => {
+	$('#timer-count').css('color', 'red');
+	$('#timer-title').css('color', 'red');
+	$('#timer-title').text("延長時間");
+});
+timer.setState(Status.Essential);	//最低通話として処理
+timer.setTimer()
