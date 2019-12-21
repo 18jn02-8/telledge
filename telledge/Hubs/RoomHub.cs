25bc8509978dd5bce23c719286061647bbc89dd2
Tatsuki <none>
2019-12-21 20:41:58 +0900

    グループ化

--- telledge/Hubs/RoomHub.cs
+++ telledge/Hubs/RoomHub.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +8,21 @@ namespace telledge.Hubs
 {
 	public class RoomHub : Hub
 	{
+		// 指定されたグループへ参加する
+		public void Join(string groupName)
+		{
+			Groups.Add(Context.ConnectionId, groupName);
+		}
+
+		// 指定されたグループから離脱する
+		public void Leave(string groupName)
+		{
+			Groups.Remove(Context.ConnectionId, groupName);
+		}
+		public void joined(int roomId)
+		{
+			Clients.Group("room_" + roomId).joined();
+		}
 		public void Hello()
 		{
 			Clients.All.hello();
