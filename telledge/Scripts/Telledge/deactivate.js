3c03038d4422ff727a1335a9bfad068fe373cc05
Tatsuki Watanabe <18jn0247@jec.ac.jp>
2019-12-23 12:27:09 +0900

    スクリプトの記述

--- telledge/Scripts/Telledge/deactivate.js
+++ telledge/Scripts/Telledge/deactivate.js
@@ -1,1 +1,13 @@
-$('.agreement-checkbox')
+$('.agreement-checkbox').click(() => {
+	isChecked = true;
+	$('.agreement-checkbox').forEach(($element) => {
+		if ($element.attr('checked') == undefined) {
+			isChecked = false;
+		}
+	});
+	if (isChecked == true) {
+		$('#inactivedate-button').attr({
+			checked: 'checked'
+		});
+	};
+});
