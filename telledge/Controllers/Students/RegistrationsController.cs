2904001d4d7b1ea12b0acb328a200e38a2153215
Tatsuki <none>
2019-12-24 16:51:56 +0900

    アクションを作成

--- telledge/Controllers/Students/RegistrationsController.cs
+++ telledge/Controllers/Students/RegistrationsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using telledge.Models;
 
 namespace telledge.Controllers.Students
 {
@@ -12,6 +13,16 @@ namespace telledge.Controllers.Students
         public ActionResult deactivate()
         {
 			return View("/Views/Students/Registrations/deactivate.cshtml");
+		}
+
+		[HttpPost]
+		public ActionResult delete()
+		{
+			if(Student.currentUser() == null) return RedirectToRoute("Student", new { controller = "Sessions", Action = "create" });
+
+			Student.currentUser().delete();
+			Student.logout();
+			return RedirectToAction("top", "Homes");
 		}
 	}
 }
