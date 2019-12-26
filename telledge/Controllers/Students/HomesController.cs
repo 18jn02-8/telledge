aa801f366865062d9458439f5497c0491a79b083
kazuyafurukawa <dsa>
2019-12-26 16:06:44 +0900

    コメントアウト追加

--- telledge/Controllers/Students/HomesController.cs
+++ telledge/Controllers/Students/HomesController.cs
@@ -22,6 +22,8 @@ namespace telledge.Controllers.Students
 		public ActionResult update(String mailaddress,String name,String imagePath,String skypeId)
 		{
 			Student student = new Student();
+			student.passwordDigest = Student.currentUser().passwordDigest;//ログイン済みパスワードダイジェスト取得
+			student.id = Student.currentUser().id;//ログイン済みのID取得
 			student.mailaddress = mailaddress;
 			student.name = name;
 			student.profileImage = imagePath;
