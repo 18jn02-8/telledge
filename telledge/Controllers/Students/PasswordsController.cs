38182ac9fad17a8d6259db23507397d2a7797682
kazuyafurukawa <dsa>
2019-12-27 14:46:45 +0900

    Updateアクションの作成

--- telledge/Controllers/Students/PasswordsController.cs
+++ telledge/Controllers/Students/PasswordsController.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using telledge.Models;
 
 namespace telledge.Controllers.Students
 {
@@ -16,6 +19,27 @@ namespace telledge.Controllers.Students
 		[HttpGet]
 		public ActionResult edit()
 		{
+			return View("/Views/Students/Passwords/edit.cshtml");
+		}
+		[HttpPost]
+		public ActionResult update(String oldPassword,String createPassword,String ConfirmationPassword)
+		{
+			Student student = Student.currentUser();
+			byte[] input = Encoding.ASCII.GetBytes(oldPassword);
+			SHA256 sha = new SHA256CryptoServiceProvider();
+			byte[] HasholdPassword = sha.ComputeHash(input);
+			if (student.passwordDigest == HasholdPassword)
+			{
+				if(createPassword == ConfirmationPassword)
+				{
+					if(createPassword != "")
+					{
+						student.setPassword(createPassword);
+						student.Update();
+						return RedirectToRoute("Student", new { controller = "Sessions", Action = "create"});
+					}
+				}
+			}
 			return View("/Views/Students/Passwords/edit.cshtml");
 		}
     }
