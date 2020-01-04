fe8dc78fd22b182c69176d82b679a0ec4785e3ec
Tatsuki <none>
2020-01-04 14:32:02 +0900

    データモデルの追加

--- telledge/Controllers/Teachers/HomesController.cs
+++ telledge/Controllers/Teachers/HomesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using telledge.Models;
 
 namespace telledge.Controllers.Teachers
 {
@@ -11,7 +12,7 @@ namespace telledge.Controllers.Teachers
         // GET: Homes
         public ActionResult mypage()
         {
-            return View("/Views/Teachers/Homes/mypage.cshtml");
+            return View("/Views/Teachers/Homes/mypage.cshtml", Teacher.currentUser());
         }
     }
 }
