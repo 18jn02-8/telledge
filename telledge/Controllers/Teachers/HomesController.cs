a3ebdc720639aff0d2aa5dfe70b49bbd039623fb
Tatsuki <none>
2020-01-06 14:08:55 +0900

    更新処理の実装

--- telledge/Controllers/Teachers/HomesController.cs
+++ telledge/Controllers/Teachers/HomesController.cs
@@ -17,9 +17,32 @@ namespace telledge.Controllers.Teachers
 
 		}
 
+		[HttpPost]
 		public ActionResult update(String imagePath ,String mailaddress ,String name, String nationality, int sex, String intoroduction)
 		{
-			return View();
+			Teacher teacher = Teacher.currentUser();
+			if (teacher != null)
+			{
+				if (mailaddress != "") teacher.mailaddress = mailaddress;
+				if (name != "") teacher.name = name;
+				if (nationality != "") teacher.nationality = nationality;
+				if (intoroduction != "") teacher.intoroduction = intoroduction;
+				teacher.sex = sex;
+
+				if (teacher.Update())
+				{
+					//正常系
+					return RedirectToRoute("teacher", new { controller = "homes", action = "mypage" });
+				}
+				else
+				{
+					//異常系
+					return RedirectToRoute("teacher", new { controller = "homes", action = "mypage" });
+				}
+			}
+			//ログインしていない場合にする処理
+			return RedirectToRoute("teacher", new { controller = "sessions", action = "create" });
+
 		}
 
 		public ActionResult edit()
