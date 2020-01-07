af6de9a7d97391b9244d1fc30e7117c84cb168ae
Tatsuki <18jn0247@jec.ac.jp>
2020-01-07 10:36:31 +0900

    データ更新処理と信号送信処理を追加

--- telledge/Hubs/RoomHub.cs
+++ telledge/Hubs/RoomHub.cs
@@ -50,9 +50,15 @@ namespace telledge.Hubs
 		//通話を終了する信号を受け取ったときに実行する処理（呼び出し元は問わない）
 		public void endCall(int roomId, int studentId)
 		{
-			//各ユーザに通話の終了を伝達する
-			Clients.Group("teacher_room_" + roomId).endCall(roomId, studentId);
-			Clients.Group("student_room_" + roomId).endCall(roomId, studentId);
+			Section section = Section.find(roomId, studentId);
+			TimeSpan talkSpan = (DateTime.Now - section.beginTime);
+			section.talkTime = talkSpan.Minutes + talkSpan.Hours * 60;
+			if (section.update())
+			{
+				//各ユーザに通話の終了を伝達する
+				Clients.Group("teacher_room_" + roomId).endCall(Room.find(roomId).getSection());
+				Clients.Group("student_room_" + roomId).endCall(roomId, studentId);
+			}
 		}
 		// 生徒がルームから退出した時の処理
 		public void leaveRoom(int roomId, int studentId)
