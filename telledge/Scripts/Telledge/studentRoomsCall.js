f6b25179750e94ea65d6220966c0cc1cadd9519a
Tatsuki <18jn0247@jec.ac.jp>
2020-01-09 09:57:46 +0900

    メソッド名を変更

--- telledge/Scripts/Telledge/studentRoomsCall.js
+++ telledge/Scripts/Telledge/studentRoomsCall.js
@@ -78,8 +78,8 @@ $(function () {
 	});
 
 	//通話する人を更新する処理
-	// updateStudent(新たに通話する生徒番号)
-	echo.on("updateStudent", (student_id) => {
+	// updateCallStudent(新たに通話する生徒番号)
+	echo.on("updateCallStudent", (student_id) => {
 		if (student_id == studentId) {
 			//通話が自分の番なら　＝　自分は通話側
 		}
